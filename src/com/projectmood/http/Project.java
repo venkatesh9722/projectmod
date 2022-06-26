0680613d6a55ac3d39053a229dfd50f4032c45bb
mind <mindtree@edureka.co>
2022-06-26 22:23:14 +0530

    commiting

---
+++ src/com/projectmood/http/Project.java
@@ -0,0 +1,9 @@
+package com.projectmood.http;
+
+public class Project {
+public static void main(String[] args) {
+	double i=70, o=35;
+	System.out.println("Addition:"+(i+o));
+	System.out.println("Substraction:"+(i-o));
+}
+}
